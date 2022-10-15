12c81b5a622ffbe318779d64f9ad069c75c3adc3
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-15 19:38:16 +0300

    added post function

--- SignUpForm.js
+++ SignUpForm.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react'
 
 
-function SignUpForm() {
+function SignUpForm( {onLogin} ) {
 
     const [ email, setEmail ] = useState('')
     const [ password, setPassword ] = useState('')
     const [ passwordConfirmation, setPasswordConfirmation ] = useState('')
     const [ errors, setErrors ] = useState([])
+
+    const url = 'http://localhost:3000/signup'
     
     function handleSubmit(e) {
         e.preventDefault()
-        fetch
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                password,
+                passwordConfirmation: passwordConfirmation,
+            }),
+        }).then((r) => {
+            if (r.ok) {
+                r.json().then((user) => onLogin(user));  
+            } else {
+                r.json() .then((err) => setErrors(err.errors))
+            }
+        })
     }
   return (
     <div></div>
