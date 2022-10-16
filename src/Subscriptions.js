92b4ec4504bef0aa0680c64cc225a5af90910128
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-16 12:01:40 +0300

    fixed subscription form

--- src/Subscriptions.js
+++ src/Subscriptions.js
@@ -38,19 +38,17 @@ function Subscriptions() {
     return (
       <section className="max-w-4xl p-6  mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
         <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
-          Add subscription here{" "}
+          Add subscription here
         </h2>
 
         <form>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
-              <label
-                className="text-gray-700 dark:text-gray-200"
-                htmlFor="name"
-              >
-                Name
-              </label>
+              <label className="text-gray-700 dark:text-gray-200">Name</label>
               <input
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
                 type="text"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               />
@@ -60,11 +58,13 @@ function Subscriptions() {
               <label
                 className="text-gray-700 dark:text-gray-200"
                 htmlFor="amount"
-                onChange={handleChange}
               >
                 Enter Amount
               </label>
-              <input
+                        <input
+                            name='amount'
+                            value={formData.amount}
+                onChange={handleChange}
                 type="text"
                 className="block w-full px-4 py-2 mt-2 text-navy-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               />
@@ -77,7 +77,9 @@ function Subscriptions() {
               >
                 Enter Start Date
               </label>
-              <input
+                        <input
+                            name='start_date'
+                            value={formData.start_date}
                 type="text"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-navy-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 onChange={handleChange}
@@ -89,20 +91,23 @@ function Subscriptions() {
                 className="text-gray-700 dark:text-gray-200"
                 htmlFor="billingCycle"
               >
-                Enter Billing Cycle{" "}
+                Enter Billing Cycle
               </label>
-              <input
+                        <input
+                            name="billing_cycle"
+                            value={ formData.billing_cycle }
+                            onChange={handleChange}
                 type="text"
-                onChange={handleChange}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               />
             </div>
           </div>
 
           <div className="flex justify-end mt-6">
-                    <button
-                        onClick={handleSubmit}
-                        className="px-8 py-2.5 leading-5 text-white transition-colors  bg-yellow duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+            <button
+              onClick={handleSubmit}
+              className="px-8 py-2.5 leading-5 text-white transition-colors  bg-yellow duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+            >
               Submit Subscription
             </button>
           </div>
